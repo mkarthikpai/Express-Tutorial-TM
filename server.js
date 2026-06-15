567d2287d5f94761291041f7b79e4ffe0c9110ac
M Karthik Pai <mkarthikpai25@gmail.com>
2026-06-15 14:47:53 +0530

    Sending HTML file

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+// Setup static folder middleware
+// app.use(express.static(path.join(__dirname, "public")));
+// Replacement is Above
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "about.html"));
+// });
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
