779babb4e69944546c990cea825abe7d3804df4b
M Karthik Pai <mkarthikpai25@gmail.com>
2026-06-17 13:17:36 +0530

    Routes and status message

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 8000;
 
 // Setup static folder middleware
 // app.use(express.static(path.join(__dirname, "public")));
@@ -9,4 +10,33 @@ const app = express();
 //   res.sendFile(path.join(__dirname, "public", "about.html"));
 // });
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+let posts = [
+  { id: 1, title: "Post1" },
+  { id: 2, title: "Post2" },
+  { id: 3, title: "Post3" },
+];
+
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  res.status(200).json(posts);
+});
+
+// Get single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `A post with id of ${id} was not found` });
+  }
+  res.status(200).json(post);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
