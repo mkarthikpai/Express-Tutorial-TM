cd288244681f22e86bcf298321ff3acc15649de8
M Karthik Pai <mkarthikpai25@gmail.com>
2026-06-17 14:07:20 +0530

    Adding the middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const posts = require("./routes/posts");
 const port = process.env.PORT || 8000;
 
 // Setup static folder middleware
@@ -10,33 +11,7 @@ const port = process.env.PORT || 8000;
 //   res.sendFile(path.join(__dirname, "public", "about.html"));
 // });
 
-let posts = [
-  { id: 1, title: "Post1" },
-  { id: 2, title: "Post2" },
-  { id: 3, title: "Post3" },
-];
-
-// Get all posts
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-  res.status(200).json(posts);
-});
-
-// Get single post
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-
-  if (!post) {
-    return res
-      .status(404)
-      .json({ msg: `A post with id of ${id} was not found` });
-  }
-  res.status(200).json(post);
-});
+//Routes
+app.use("/api/posts", posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
